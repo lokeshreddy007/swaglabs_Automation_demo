b875032c9850c1e1fed5f2f54f6aba370f13e0e8
lokeshreddy007 <lokeshreddy160@gmail.com>
2020-07-12 21:36:17 +0530

    add Allure

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,17 @@
 require 'capybara/cucumber'
 require 'pry'
 require 'rspec'
+require 'allure-cucumber'
+# require 'capybara/dsl'
 
 Capybara.default_driver = :selenium_chrome
+# include AllureCucumber::DSL
+# ​attach_file(title, file)
+
+AllureCucumber.configure do |c|
+    c.clean_dir  = false
+end
+
+  
+
+# https://docs.qameta.io/allure/#_cucumber
